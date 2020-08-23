64bd077379c8bf211e1e51383967b6b270338386
Hakkeray <rukeine@gmail.com>
2020-08-22 20:31:10 -0400

    removed dash bootstrap

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import dash_bootstrap_components as dbc
+#import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output, State
 import pandas as pd
 # PLOTLY / CUFFLINKS for iplots
